7c6fdce62e0060444ca6f0d4ffd08ba19f0489c7
Aiman Fadillah <mlakece914@gmail.com>
2024-03-08 09:41:51 +0700

    memperbaiki filter anime

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ dotenv.config();
 
 const app = express();
 const bot = new TelegramBot(process.env.TOKEN_BOT, { polling: true });
-let oldAnime = [];
 const chatID = [];
-let server = false;
+
+const response = await axios.get("https://web-anime-psi.vercel.app/anime?page=1&type=ongoing");
+let oldAnime = response.data.reverse();
 
 async function getNewAnime() {
     const response = await axios.get("https://web-anime-psi.vercel.app/anime?page=1&type=ongoing");
-    const newAnime = response.data.reverse();
-    const filterAnime = oldAnime.length > 0 ? newAnime.filter((anime, index) => anime.slug != oldAnime[index].slug) : newAnime;
-    oldAnime = newAnime
+    let newAnime = response.data.reverse();
+    if(oldAnime.length > 0){
+        for(const old of oldAnime){
+            newAnime = newAnime.filter((anime) => anime.slug !== old.slug);
+        }
+    }else{
+        oldAnime = newAnime
+    }
     chatID.forEach((id) => {
-        filterAnime.forEach((anime) => {
+        newAnime.forEach((anime) => {
             bot.sendPhoto(id, `https://mangapi-man.vercel.app/gambar?url=${anime.gambar}`, {
                 caption: `
                 ${anime.judul} Episode ${anime.eps[1]}
@@ -31,16 +37,17 @@ https://animan.fun/anime/${anime.slug}
 }
 
 const job = new CronJob('0 0 0 * * *', getNewAnime, null, true, 'Asia/Jakarta');
-        bot.on("message", (msg) => {
-            if (msg.text === "/start") {
-                const id = msg.chat.id
-                bot.sendMessage(id, `Selamat datang ${msg.chat.first_name} saya akan memberikan update anime setiap 1 hari sekali`)
-                const checkId = chatID.filter((chat) => chat == id);
-                if (checkId.length == 0) {
-                    chatID.push(id)
-                    console.log(`New user ${id}`)
-                }
-            }
+
+bot.on("message", (msg) => {
+    if (msg.text === "/start") {
+        const id = msg.chat.id
+        bot.sendMessage(id, `Selamat datang ${msg.chat.first_name} saya akan memberikan update anime setiap 1 hari sekali`)
+        const checkId = chatID.filter((chat) => chat == id);
+        if (checkId.length == 0) {
+            chatID.push(id)
+            console.log(`New user ${id}`)
+        }
+    }
 })
 
 app.get("*",(req,res) => {
